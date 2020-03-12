967d8bb4c590674c96695e0ed2fabf725750ae55
Martin Stolarik <stolarik6@uniba.sk>
2020-03-12 17:23:44 +0100

    functions #9 - #12

--- stolarik.py
+++ stolarik.py
@@ -116,3 +116,49 @@ class Relation:
                             newRelation = newRelation.add(x,y2)
         return newRelation
 
+    #9 returns true if relation is reflexve
+    def isReflexive(self):
+        for x in self.values:
+            if not(self.has(x,x)):
+                    return False
+        return True
+
+    #10 returns true if relation is symetric
+    def isSymertic(self):
+        for x in self.values:
+            for y in self.values:
+                if self.has(x,y):
+                    if not(self.has(y,x)):
+                        return False
+        return True
+
+    #11 returns true if relation is transitive
+    def isTransitive(self):
+        for x in self.values:
+            for y in self.values:
+                if self.has(x,y):
+                    for y1 in self.values:
+                        if self.has(y,y1):
+                            if not(self.has(x,y1)):
+                                return False
+        return True
+
+    #12 returns reflexive-transitive closure
+    def getClosure(self):
+        newRelation = self
+        #add all (x,x) elements = make relation reflexive
+        for x in self.values:
+            newRelation = newRelation.add(x,x)
+        #make newRelation transitive
+        newElement = True
+        while newElement:
+            newElement = False
+            for x in self.values:
+                for y in self.values:
+                    if newRelation.has(x,y):
+                        for y1 in self.values:
+                            if newRelation.has(y,y1):
+                                if not(newRelation.has(x,y1)):
+                                    newRelation = newRelation.add(x,y1)
+                                    newElement = True
+        return newRelation
