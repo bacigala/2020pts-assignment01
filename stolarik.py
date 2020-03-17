6ccd1753909b54ce6416c0c553931f63a58f9ea4
bacigala <60620588+bacigala@users.noreply.github.com>
2020-03-17 16:00:21 +0100

    fixed reviewed issues

--- stolarik.py
+++ stolarik.py
@@ -1,168 +1,192 @@
 from pyrsistent import pvector
 
-class Relation:
-    #immutable pvector of values
+def get_relation_class(inputSet):
+
+    #immutable pvector of __values
     values = pvector()
     #pvector of pvectors of booleans = represents relations
-    relations = pvector()
-
-    def __init__(self, inputSet, relations = False):
-        #fill values set -> immutable pvector
-        for x in inputSet:
-            self.values = self.values.append(x)
-        #create 2D immutable matrix for relations
-        if not relations:
-            for x in range(len(self.values)):
-                subRelations = pvector()
-                for y in range(len(self.values)):
-                    subRelations = subRelations.append(False)
-                self.relations = self.relations.append(subRelations)
-        else:
-            self.relations = relations
-
-    #finds index of element
-    def indexOf(self, x):
-        if (x in self.values):
-            return self.values.index(x)
-        else:
-            return -1
+    initRelations = pvector()
     
-    #returns empty Relation with united values with self
-    def unitedValuesRelation(self, otherRelation):
-        newValues = pvector()
-        for x in self.values:
-            newValues = newValues.append(x)
-        for x in otherRelation.values:
-            if not (x in newValues):
+    #fill __values set -> immutable pvector
+    for x in inputSet:
+        values = values.append(x)
+    #create 2D immutable matrix for relations
+    for x in range(len(values)):
+        subRelations = pvector()
+        for y in range(len(values)):
+            subRelations = subRelations.append(False)
+        initRelations = initRelations.append(subRelations)
+    
+    class Relation:
+        #immutable pvector of __values
+        __values = values
+        #pvector of pvectors of booleans = represents relations
+        __relations = initRelations
+
+        def __init__(self, inputSet = values, inputRelations = False):
+            #fill __values set -> immutable pvector
+            self.__values = pvector()
+            for x in inputSet:
+                self.__values = self.__values.append(x)
+            #create 2D immutable matrix for relations
+            if not inputRelations:
+                self.__relations = pvector()
+                for x in range(len(self.__values)):
+                    subRelations = pvector()
+                    for y in range(len(self.__values)):
+                        subRelations = subRelations.append(False)
+                    self.__relations = self.__relations.append(subRelations)
+            else:
+                self.__relations = inputRelations
+    
+        #returns immutable pvector of values
+        def getValues(self):
+            return self.__values
+
+        #finds index of element
+        def indexOf(self, x):
+            if (x in self.__values):
+                return self.__values.index(x)
+            else:
+                return -1
+    
+        #returns empty Relation with united __values with self
+        def unitedValuesRelation(self, otherRelation):
+            newValues = pvector()
+            for x in self.__values:
                 newValues = newValues.append(x)
-        return Relation(newValues)
+            otherRelationValues = otherRelation.getValues()
+            for x in otherRelationValues:
+                if not (x in newValues):
+                    newValues = newValues.append(x)
+            return Relation(newValues)
+    
 
+        #1 returns true if x and y are in relation
+        def has(self, x, y):
+            if (x in self.__values) and (y in self.__values):
+                indexX = self.__values.index(x)
+                indexY = self.__values.index(y)
+                return self.__relations[indexX][indexY]
+            else:
+                return False
 
-    #1 returns true if x and y are in relation
-    def has(self, x, y):
-        if (x in self.values) and (y in self.values):
-            indexX = self.values.index(x)
-            indexY = self.values.index(y)
-            return self.relations[indexX][indexY]
-        else:
-            return False
-
-    #2 adds element (x,y) to relation
-    def add(self, x, y):
-        indexX = self.indexOf(x)
-        indexY = self.indexOf(y)
-        if (indexX > -1) and (indexY > -1):
-            newSubRelations = self.relations[indexX].set(indexY, True)
-            return Relation(self.values, self.relations.set(indexX, newSubRelations))
-        else:
-            return self
-
-    #3 removes element (x,y) form relation
-    def remove(self, x, y):
-        if self.has(x,y):
+        #2 adds element (x,y) to relation
+        def add(self, x, y):
             indexX = self.indexOf(x)
             indexY = self.indexOf(y)
-            newSubRelations = self.relations[indexX].set(indexY, False)
-            return Relation(self.values, self.relations.set(indexX, newSubRelations))
+            if (indexX > -1) and (indexY > -1):
+                newSubRelations = self.__relations[indexX].set(indexY, True)
+                return Relation(self.__values, self.__relations.set(indexX, newSubRelations))
+            else:
+                return self
+    
+        #3 removes element (x,y) form relation
+        def remove(self, x, y):
+            if self.has(x,y):
+                indexX = self.indexOf(x)
+                indexY = self.indexOf(y)
+                newSubRelations = self.__relations[indexX].set(indexY, False)
+                return Relation(self.__values, self.__relations.set(indexX, newSubRelations))
+    
+        #4 returns union of two relations
+        def union(self, otherRelation):
+            newRelation = self.unitedValuesRelation(otherRelation)
+            otherRelationValues = otherRelation.getValues()
+            for x in self.__values:
+                for y in self.__values:
+                    if self.has(x,y):
+                        newRelation = newRelation.add(x,y)
+            for x in otherRelationValues:
+                for y in otherRelationValues:
+                    if otherRelation.has(x,y):
+                        newRelation = newRelation.add(x,y)
+            return newRelation
+    
+        #5 returns intrersection of two relations
+        def intersection(self, otherRelation):
+            newRelation = self.unitedValuesRelation(otherRelation)
+            for x in self.__values:
+                for y in self.__values:
+                    if self.has(x,y) and otherRelation.has(x,y):
+                        newRelation = newRelation.add(x,y)
+            return newRelation
 
-    #4 returns union of two relations
-    def union(self, otherRelation):
-        newRelation = self.unitedValuesRelation(otherRelation)
-        for x in self.values:
-            for y in self.values:
-                if self.has(x,y):
-                    newRelation = newRelation.add(x,y)
-        for x in otherRelation.values:
-            for y in otherRelation.values:
-                if otherRelation.has(x,y):
-                    newRelation = newRelation.add(x,y)
-        return newRelation
+        #6 returns subtraction of two relations
+        def subtract(self, otherRelation):
+            newRelation = self.unitedValuesRelation(otherRelation)
+            for x in self.__values:
+                for y in self.__values:
+                    if self.has(x,y) and not(otherRelation.has(x,y)):
+                        newRelation = newRelation.add(x,y)
+            return newRelation
 
-    #5 returns intrersection of two relations
-    def intersection(self, otherRelation):
-        newRelation = self.unitedValuesRelation(otherRelation)
-        for x in self.values:
-            for y in self.values:
-                if self.has(x,y) and otherRelation.has(x,y):
-                    newRelation = newRelation.add(x,y)
-        return newRelation
+        #7 returns inverse relation
+        def inverse(self):
+            newRelation = self.unitedValuesRelation(self)
+            for x in self.__values:
+                for y in self.__values:
+                    if not(self.has(x,y)):
+                        newRelation = newRelation.add(x,y)
+            return newRelation
 
-    #6 returns subtraction of two relations
-    def subtract(self, otherRelation):
-        newRelation = self.unitedValuesRelation(otherRelation)
-        for x in self.values:
-            for y in self.values:
-                if self.has(x,y) and not(otherRelation.has(x,y)):
-                    newRelation = newRelation.add(x,y)
-        return newRelation
+        #8 returns composition of 2 relations
+        def compose(self, otherRelation):
+            otherRelationValues = otherRelation.getValues()
+            newRelation = self.unitedValuesRelation(otherRelation)
+            for x in self.__values:
+                for y in self.__values:
+                    if self.has(x,y):
+                        for y2 in otherRelationValues:
+                            if otherRelation.has(y,y2):
+                                newRelation = newRelation.add(x,y2)
+            return newRelation
 
-    #7 returns inverse relation
-    def inverse(self):
-        newRelation = self.unitedValuesRelation(self)
-        for x in self.values:
-            for y in self.values:
-                if not(self.has(x,y)):
-                    newRelation = newRelation.add(x,y)
-        return newRelation
-
-    #8 returns composition of 2 relations
-    def compose(self, otherRelation):
-        newRelation = self.unitedValuesRelation(otherRelation)
-        for x in self.values:
-            for y in self.values:
-                if self.has(x,y):
-                    for y2 in otherRelation.values:
-                        if otherRelation.has(y,y2):
-                            newRelation = newRelation.add(x,y2)
-        return newRelation
-
-    #9 returns true if relation is reflexve
-    def isReflexive(self):
-        for x in self.values:
-            if not(self.has(x,x)):
-                    return False
-        return True
-
-    #10 returns true if relation is symetric
-    def isSymertic(self):
-        for x in self.values:
-            for y in self.values:
-                if self.has(x,y):
-                    if not(self.has(y,x)):
+        #9 returns true if relation is reflexve
+        def isReflexive(self):
+            for x in self.__values:
+                if not(self.has(x,x)):
                         return False
-        return True
+            return True
+    
+        #10 returns true if relation is symetric
+        def isSymertic(self):
+            for x in self.__values:
+                for y in self.__values:
+                    if self.has(x,y):
+                        if not(self.has(y,x)):
+                            return False
+            return True
 
-    #11 returns true if relation is transitive
-    def isTransitive(self):
-        for x in self.values:
-            for y in self.values:
-                if self.has(x,y):
-                    for y1 in self.values:
-                        if self.has(y,y1):
-                            if not(self.has(x,y1)):
-                                return False
-        return True
+        #11 returns true if relation is transitive
+        def isTransitive(self):
+            for x in self.__values:
+                for y in self.__values:
+                    if self.has(x,y):
+                        for y1 in self.__values:
+                            if self.has(y,y1):
+                                if not(self.has(x,y1)):
+                                    return False
+            return True
 
-    #12 returns reflexive-transitive closure
-    def getClosure(self):
-        newRelation = self
-        #add all (x,x) elements = make relation reflexive
-        for x in self.values:
-            newRelation = newRelation.add(x,x)
-        #make newRelation transitive
-        newElement = True
-        while newElement:
-            newElement = False
-            for x in self.values:
-                for y in self.values:
-                    if newRelation.has(x,y):
-                        for y1 in self.values:
-                            if newRelation.has(y,y1):
-                                if not(newRelation.has(x,y1)):
-                                    newRelation = newRelation.add(x,y1)
-                                    newElement = True
-        return newRelation
+        #12 returns reflexive-transitive closure
+        def getClosure(self):
+            newRelation = self
+            #add all (x,x) elements = make relation reflexive
+            for x in self.__values:
+                newRelation = newRelation.add(x,x)
+            #make newRelation transitive
+            newElement = True
+            while newElement:
+                newElement = False
+                for x in self.__values:    
+                    for y in self.__values:
+                        if newRelation.has(x,y):
+                            for y1 in self.__values:
+                                if newRelation.has(y,y1):
+                                    if not(newRelation.has(x,y1)):
+                                        newRelation = newRelation.add(x,y1)
+                                        newElement = True
+            return newRelation
 
-#returns class representing relations on a given set
-def get_relation_class(inputSet):
-    return Relation(inputSet)
+    return Relation
